60c26d856ad0f6ec25675c8d0666a00fd8f077e1
Tomas <tomasuhiaotero@gmail.com>
2025-12-05 23:02:04 +0100

    Controller Model thymeleaf

--- springboot-applications/src/main/java/com/tomas/spring/app1/springboot_applications/controllers/EjemploController.java
+++ springboot-applications/src/main/java/com/tomas/spring/app1/springboot_applications/controllers/EjemploController.java
@@ -1,6 +1,7 @@
 package com.tomas.spring.app1.springboot_applications.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,7 +12,10 @@ public class EjemploController {
     @GetMapping("/detalles_info")
     
     
-    public String info() {
+    public String info(Model model) {
+        model.addAttribute("Titulo", "Servidor en linea");
+        model.addAttribute("Servidor", "ServerTomas");
+        model.addAttribute("Ip", "192.168.1.1");
         return "detalles_info";
     }
 }
